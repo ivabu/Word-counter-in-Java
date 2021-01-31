4262d88211e6d13802aced7eca9893d129c2e803
Iva Simon Bubalo <53701883+ivabu@users.noreply.github.com>
2021-01-31 18:14:05 +0000

    Changed IF statement

--- Driver.java
+++ Driver.java
@@ -10,18 +10,17 @@ public class Driver {
         scan.close();
         userInput = userInput.trim(); //clean by removing leading and lagging spaces
         int count = 0;
-        if (userInput.length() == 0){
-            count = 0;
-        }
-        else {
-            //count in the first word (other words defined by spaces at the beginning and end)
-            //first word cannot begin with a space
-            count++;
-            for(int i = 0; i < userInput.length(); i++) {
-                if (userInput.charAt(i) == ' '
-                        && userInput.charAt(i + 1) != ' '){
-                    // ' ?' pattern count as 1
-                    count++;
+        if (userInput.length() != 0) {
+            {
+                //count in the first word (other words defined by spaces at the beginning and end)
+                //first word cannot begin with a space
+                count++;
+                for(int i = 0; i < userInput.length(); i++) {
+                    if (userInput.charAt(i) == ' '
+                            && userInput.charAt(i + 1) != ' '){
+                        // ' ?' pattern count as 1
+                        count++;
+                    }
                 }
             }
         }
